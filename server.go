afdb0665f96395715e28d5e99e37e4053695993a
xushiyu <xushirain@163.com>
2021-09-06 23:19:15 +0800

    ver2 用户上线功能

--- server.go
+++ server.go
@@ -3,25 +3,66 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	IP   string
 	Port int
+
+	//在线用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	//消息广播的channel
+	Message chan string
 }
 
 //创建一个Server的接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		IP:   ip,
-		Port: port,
+		IP:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
+//监听Message
+func (server *Server) ListenMessage() {
+	for {
+		msg := <-server.Message
+
+		//将msg发送给全部在线的User
+		server.mapLock.Lock()
+		for _, cli := range server.OnlineMap {
+			cli.C <- msg
+		}
+		server.mapLock.Unlock()
+	}
+}
+
+//广播消息的方法
+func (server *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	server.Message <- sendMsg
+}
+
 func (server *Server) Handler(conn net.Conn) {
 	//当前连接业务
-	fmt.Println("链接建立成功")
+	//fmt.Println("链接建立成功")
+	user := NewUser(conn)
+	//用户上线，将用户加入到onlineMap中
+	server.mapLock.Lock()
+	server.OnlineMap[user.Name] = user
+	server.mapLock.Unlock()
+
+	//广播当前用户上线消息
+	server.BroadCast(user, "已上线")
+
+	//当前Handler阻塞
+	select {}
 }
 
 //启动服务器的端口
@@ -34,6 +75,9 @@ func (server *Server) Start() {
 	}
 	//close listen socket
 	defer listener.Close()
+
+	go server.ListenMessage()
+
 	for {
 		//accept
 		conn, err := listener.Accept()
